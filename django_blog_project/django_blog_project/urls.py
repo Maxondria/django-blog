aac4152eaabd04566a44ad7797b5b676598d160c
Maxondria <maxtayebwa@gmail.com>
2020-02-21 15:21:41 +0300

    +feat(): Perform password reset

--- django_blog_project/django_blog_project/urls.py
+++ django_blog_project/django_blog_project/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import (LoginView, LogoutView,
+                                       PasswordResetCompleteView,
+                                       PasswordResetConfirmView,
+                                       PasswordResetDoneView,
+                                       PasswordResetView)
 from django.urls import include, path
 
 from users import views as user_views
@@ -27,6 +31,14 @@ urlpatterns = [
     path('profile/', user_views.profile, name='profile'),
     path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('password-reset/', PasswordResetView.as_view(
+        template_name='users/password_reset.html'), name='password_reset'),
+    path('password-reset/done/', PasswordResetDoneView.as_view(
+        template_name='users/password_reset_done.html'), name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(
+        template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('password-reset-complete/', PasswordResetCompleteView.as_view(
+        template_name='users/password_reset_complete.html'), name='password_reset_complete'),
     path('', include('blog.urls')),
 ]
 
